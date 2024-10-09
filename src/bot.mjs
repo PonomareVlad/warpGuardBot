52bd59352f8e8367d9f5a2086a1a3c33ec2f6723
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-10-09 16:59:57 +0200

    add extra commands

--- src/bot.mjs
+++ src/bot.mjs
@@ -9,5 +9,7 @@ export const {
 export const bot = new Bot(token)
 const safe = bot.errorBoundary(console.error)
 
+safe.command('ls', async ctx => ctx.reply(await exec('ls')))
+safe.command('pwd', async ctx => ctx.reply(await exec('pwd')))
 safe.command('version', async ctx => ctx.reply(await exec('./bin/wg -v')))
 safe.on('message:text', ctx => ctx.reply(ctx.msg.text))
