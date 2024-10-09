f842ef02a3f2f4d6c373c608c5fae004d794e006
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-10-09 16:34:09 +0200

    try to copy wg binary

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,5 +1,5 @@
 import { Bot } from 'grammy'
-import { checkWgIsInstalled } from 'wireguard-tools'
+import { exec } from 'wireguard-tools/dist/utils/exec'
 
 export const {
   TELEGRAM_BOT_TOKEN: token,
@@ -9,5 +9,5 @@ export const {
 export const bot = new Bot(token)
 const safe = bot.errorBoundary(console.error)
 
-safe.command('version', async ctx => ctx.reply(await checkWgIsInstalled()))
+safe.command('version', async ctx => ctx.reply(await exec('./src/wg -v')))
 safe.on('message:text', ctx => ctx.reply(ctx.msg.text))
