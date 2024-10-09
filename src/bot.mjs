477906b6d8f03fbb73ad10f0d561e34549aa7ba5
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-10-09 20:06:22 +0200

    full prototype

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,5 +1,9 @@
+import {
+  generateKeyPair,
+  checkWgIsInstalled,
+  generateConfigString,
+} from 'wireguard-tools'
 import { Bot } from 'grammy'
-import { checkWgIsInstalled, generateKeyPair } from 'wireguard-tools'
 
 export const {
   CLOUDFLARE_API_URL: api,
@@ -7,17 +11,75 @@ export const {
   TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(':').pop(),
 } = process.env
 
+const defaultHeaders = new Headers({
+  'content-type': 'application/json',
+  'user-agent': '',
+})
+
 export const bot = new Bot(token)
 
 const safe = bot.errorBoundary(console.error)
 
 safe.command('version', async ctx => ctx.reply(await checkWgIsInstalled()))
-safe.command('keys', async ctx => {
-  const { publicKey, privateKey, preSharedKey } = await generateKeyPair({
-    preSharedKey: true,
+
+safe.command('start', async ctx => {
+  const { publicKey, privateKey } = await generateKeyPair()
+  const date = new Date()
+  date.setMilliseconds(0)
+  const {
+    result: { id, token },
+  } = await fetch(new URL('reg', api), {
+    body: JSON.stringify({
+      install_id: '',
+      tos: date,
+      key: publicKey,
+      fcm_token: '',
+      type: 'ios',
+      locale: 'en_US',
+    }),
+    headers: defaultHeaders,
+    method: 'POST',
+  }).then(response => response.json())
+  const headers = new Headers(defaultHeaders)
+  headers.set('authorization', `Bearer ${token}`)
+  const {
+    result: {
+      config: {
+        peers: [
+          {
+            public_key,
+            endpoint: { host },
+          },
+        ],
+        interface: {
+          addresses: { v4, v6 },
+        },
+      },
+    },
+  } = await fetch(new URL(`reg/${id}`, api), {
+    body: JSON.stringify({ warp_enabled: true }),
+    method: 'PATCH',
+    headers,
+  }).then(response => response.json())
+  const { port, hostname } = new URL(`https://${host}`)
+  const config = generateConfigString({
+    wgInterface: {
+      privateKey,
+      address: [v4, v6],
+      dns: [
+        '1.1.1.1',
+        '2606:4700:4700::1111',
+        '1.0.0.1',
+        '2606:4700:4700::1001',
+      ],
+    },
+    peers: [
+      {
+        publicKey: public_key,
+        allowedIps: ['0.0.0.0/1', '128.0.0.0/1', '::/1', '8000::/1'],
+        endpoint: `${hostname}:${port}`,
+      },
+    ],
   })
-  await ctx.reply(publicKey)
-  await ctx.reply(privateKey)
-  await ctx.reply(preSharedKey)
+  await ctx.reply(config)
 })
-safe.on('message:text', ctx => ctx.reply(ctx.msg.text))
